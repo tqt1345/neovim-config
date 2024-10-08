c7b13084311326ec56ae33f23bb7a2d693ae548f
tqt <tqt1345@gmail.com>
2024-10-08 19:06:45 +1100

    update config
    set colour scheme to everforest
    disabled inline hints by default

---
+++ lua/plugins/core.lua
@@ -0,0 +1,16 @@
+return {
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+}
