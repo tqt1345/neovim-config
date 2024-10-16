10bf65063aad1af1afb339e36a2bfe09dcd8ce68
tqt <tqt1345@gmail.com>
2024-10-17 09:16:07 +1100

    feat: add colour scheme, disabled plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Set the colorscheme
+-- vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[colorscheme ayu]])
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,16 +1,29 @@
 return {
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
+  -- COLOUR SHCEME
 
+  -- Install colorschemes
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  { "ayu-theme/ayu-vim" },
+
+  -- -- select colorscheme
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-mocha",
+  --   },
+  -- },
+
+  -- DISABLE & OR OVERRIDE PLUGINS
+
+  -- Disable inlay hints by default (type hints)
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
     },
   },
+
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
